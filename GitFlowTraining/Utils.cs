690e9611d62660cf27a9c7cd42185206dc504e75
IvanPostu <ipostu20000127@gmail.com>
2022-06-14 14:56:25 +0300

    add isOdd method

---
+++ GitFlowTraining/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFlowTraining
+{
+    class Utils
+    {
+        public static bool isOdd(int a)
+        {
+            return a % 2 == 1;
+        }
+    }
+}
